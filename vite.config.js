792e74315f7573bdd1a7963912be6b9597fc3497
notVinh <dqv4work@gmail.com>
2024-03-31 19:09:27 +0700

    add api

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://v-blog-api.vercel.app/",
         secure: false,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\//, ""),
